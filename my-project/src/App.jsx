23f972704bc5ddec8399295467a06775831fbb8d
AmreliyaAakash <amreliyaaakash3@gmail.com>
2026-02-12 00:24:51 +0530

    in Production

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import Hero from './sections/Hero.jsx'
 import ShowCaseSection from './sections/ShowCaseSection.jsx'
+import Navbar from './components/Navbar.jsx'
+import LogoSection from './components/LogoSection.jsx'
+import FeatureCards from './sections/FeatureCards.jsx'
+import Experience from './sections/Experience.jsx'
 
 const App = () => {
   return (
   <>
+    <Navbar/>
     <Hero/>
     <ShowCaseSection/>
+    <LogoSection/>
+    <FeatureCards/>
+    <Experience/>
   </>
   )
 }
--- my-project/src/components/models/hero_models/HeroExperience.jsx
+++ my-project/src/components/models/hero_models/HeroExperience.jsx
@@ -4,36 +4,40 @@ import { useMediaQuery } from 'react-responsive'
 import HeroLight from './HeroLight.jsx'
 import { Room } from './Room.jsx'
 import Particles from './Particles.jsx'
+import { Suspense } from 'react'
 import React from 'react'
 
 
 const HeroExperience = () => {
-    const istablet = useMediaQuery({ query: '(max-width: 1024px)' }); 
-    const ismobile = useMediaQuery({ query: '(max-width: 768px)' }); 
-    return (
-    
+  const istablet = useMediaQuery({ query: '(max-width: 1024px)' });
+  const ismobile = useMediaQuery({ query: '(max-width: 768px)' });
+  return (
+
     <Canvas camera={{ position: [50, 5, -1], fov: 30 }}>
-        
-         <OrbitControls
-            enableZoom={true}
-            enablePan={!istablet}
-            maxDistance={20}
-            minDistance={5}
-            minPolarAngle={Math.PI / 4}
-            maxPolarAngle={Math.PI / 2}
 
-         />
-            <HeroLight/>
-            <Particles count={150} />
-            <group 
-            scale={ ismobile? 0.7 : 1} 
-            position = {[0,-3.5,0]}
-            rotation={[0, Math.PI / 4, 0]}
-            >
-                <Room />
-            </group>
+      <OrbitControls
+        enableZoom={true}
+        enablePan={!istablet}
+        maxDistance={20}
+        minDistance={5}
+        minPolarAngle={Math.PI / 4}
+        maxPolarAngle={Math.PI / 2}
+      />
 
-       
+      <HeroLight />
+      <Particles count={50} />
+      <group
+        scale={ismobile ? 0.7 : 1}
+        position={[0, -3.5, 0]}
+        rotation={[0, Math.PI / 4, 0]}
+      >
+        <Suspense fallback={null}>
+          <Room />
+        </Suspense>
+
+      </group>
+
+
     </Canvas>
   )
 }
